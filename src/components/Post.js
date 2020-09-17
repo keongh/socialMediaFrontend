1c48321eb57096d0c644a0a139a4f625aa8cc1ab
keongh <kghadamyari@gmail.com>
2020-09-17 14:43:31 -0400

    Feature/comment box on dashboard (#3)
    * Added comment box to dashboard
    
    * Show comment and like count on each post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import NewComment from './NewComment.js'
 
 export default class Post extends Component {
 
@@ -101,6 +102,14 @@ export default class Post extends Component {
         </div>
       );
     }
+    let pluralLikes = "";
+    if (this.props.nLikes !== 1) {
+      pluralLikes = "s";
+    }
+    let pluralComments = "";
+    if (this.props.nComments !== 1) {
+      pluralComments = "s";
+    }
 
     return (
       <div className="media text-muted pt-3">
@@ -111,21 +120,27 @@ export default class Post extends Component {
           <rect width="100%" height="100%" fill="#007bff"></rect>
           <text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text>
         </svg>
+        <div className="ml-3">
+          <Link to={`/dashboard/post/${this.props.id}`}> <small>{this.props.nLikes} Like{pluralLikes} {this.props.nComments} Comment{pluralComments}</small> </Link>
+        </div>
         <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
           <strong className="d-block text-gray-dark">{this.props.author.userName}</strong>
-          "
-          <Link to={`/dashboard/post/${this.props.id}`}>{this.props.text}</Link>
-          "
+
+          {this.props.text}
+
+        <div className="collapse pt-3" id="commentBox">
+          <NewComment id={this.props.id} action={this.props.updateHandler}/>
+        </div>
+
         </p>
         {heartIcon}
         {followIcon}
 
-        <svg class="bi bi-chat" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <svg class="bi bi-chat" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" data-toggle="collapse" data-target="#commentBox">
           <path fill-rule="evenodd" d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
         </svg>
 
         {trashcan}
-
       </div>
     );
   }
